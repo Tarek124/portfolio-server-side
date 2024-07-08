6c9d6c7240598da6005111adb02893402a36dff4
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-07-08 21:36:07 +0600

    deleted method added

--- projectHandler.js
+++ projectHandler.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
   } catch (err) {
     res.status(500).json({ err, message: "there was a server-side error" });
   }
-});
+})
 router.delete("/", async (req, res) => {
   const { id } = req.query;
   try {
