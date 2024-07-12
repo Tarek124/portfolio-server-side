20d18082a25221074184d7f3fcd5bb1eacc8f4ac
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-07-13 00:06:24 +0600

    bug fixed

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://tarek124.netlify.app", "http://localhost:5173"],
+  })
+);
 
 const uri = `mongodb+srv://${process.env.name}:${process.env.pass}@cluster0.83drhwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -16,7 +21,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/project", projectHandler);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("App listening on port 3000");
 });
